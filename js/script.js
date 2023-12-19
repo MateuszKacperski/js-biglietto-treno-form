ccaba1633fc88b7fe8f9b1314249936cd63d3ac6
MateuszKacperski <kacperskimateusz95@gmail.com>
2023-12-19 16:43:29 +0100

    added milestone 2

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ console.log('JS OK');
 const km = document.getElementById('km');
 const eta = document.getElementById('age');
 const button = document.getElementById('button');
+const prezzo = document.getElementById('prezzo');
+let carozza = document.getElementById('carozza');
+let offerta = document.getElementById('offerta');
+let nbiglietto = document.getElementById('nbiglietto');
 
 
 //Aggiungo un evento sul click del bottone
@@ -30,72 +34,27 @@ button.addEventListener('click', function(){
         const sconto40 = (40 * price) / 100 ;
         price = price - sconto40;
         console.log(price.toFixed(2));
+        prezzo.innerText = price.toFixed(2) + '€';
+        offerta.innerText = `${offerta}Biglietto scontato 40%`;
     }else if(eta === 'minorenne'){
         const sconto20 = (20 * price) / 100 ;
         price = price - sconto20;
         console.log(price.toFixed(2));
+        prezzo.innerText = price.toFixed(2) + '€';
     }else {
         console.log(price);
+        prezzo.innerText = price.toFixed(2) + '€';
     }
 
-   
+    //Creo il numero carozza
 
+    const numeroCarozza = Math.floor(Math.random()*12)+1;
+    carozza.innerText = carozza + numeroCarozza;
+
+
+    //Creo Numero biglietto
+    const numeroBiglietto = Math.floor(Math.random()*10000)+1;
+    nbiglietto.innerText = nbiglietto + numeroBiglietto;
 })
 
 
-/*
-//Chiedere all'utente i chilometri da percorrere
-
-const chilometri = parseInt(prompt('Quanti chilometri devi percorrere ?', '10'));
-
-// Chidere all'utente la sua età
-
-const eta = parseInt(prompt('Quanti anni hai ?', '66'));
-
-console.log(chilometri, eta);
-
-//validazione
-
-if(isNaN(eta) || isNaN(chilometri) || eta <= 0 || chilometri <= 0){
-    alert('I dati inseriti non sono corretti');
-}
-
-//Creo costante prezzo biglietto per chilometro
-const perkm = 0.21 ;
-
-
-//calcolo il prezzo del biglietto
-let prezzobiglietto = chilometri * perkm ;
-
-console.log(prezzobiglietto);
-
-//Sconto del 20%
-
-const sconto20 = (20 * prezzobiglietto) / 100 ;
-
-console.log(sconto20);
-
-//Sconto del 40%
-
-const sconto40 = (40 * prezzobiglietto) / 100 ;
-
-console.log(sconto40);
-
-
-//Controllo se lo sconto va applicato
-
-if(eta < 18){
-    prezzobiglietto = prezzobiglietto - sconto20 ;
-}else if(eta > 65) {
-    prezzobiglietto = prezzobiglietto - sconto40 ;
-}
-
-//Tolgo i decimali in piu
-prezzobiglietto = prezzobiglietto.toFixed(2);
-
-
-console.log(prezzobiglietto);
-
-
-//Stampo il prezzo finale del biglietto
-document.getElementById('result').innerText += prezzobiglietto + '€';  */
